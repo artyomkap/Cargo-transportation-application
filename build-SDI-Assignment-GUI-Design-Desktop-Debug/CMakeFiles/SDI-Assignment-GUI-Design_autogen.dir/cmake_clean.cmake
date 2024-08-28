cb5acb3bd76e06bd462fb6692392bbf42a5aa7cb
Artem Kapustin <103292137+artyomkap@users.noreply.github.com>
2024-08-28 14:16:40 +0100

    Delete build-SDI-Assignment-GUI-Design-Desktop-Debug directory

--- build-SDI-Assignment-GUI-Design-Desktop-Debug/CMakeFiles/SDI-Assignment-GUI-Design_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/SDI-Assignment-GUI-Design_autogen"
-  "SDI-Assignment-GUI-Design_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SDI-Assignment-GUI-Design_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
